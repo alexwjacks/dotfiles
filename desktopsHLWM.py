fe11bd6f01e55859a50e13219de6ab2f186011e2
Alex Jacks <ajacks@ajacks-dt.archivas.com>
2016-06-09 08:58:57 -0400

    Replaced dzen with lemonbar for HLWM

---
+++ desktopsHLWM.py
@@ -0,0 +1,31 @@
+import subprocess
+
+call = ["herbstclient", "tag_status", "0"]
+desktops = subprocess.check_output(call)
+desktop_list = desktops.split("\t")
+desktop_list = desktop_list[1:-1]
+#print(desktop_list)
+
+total_string = ""
+
+for desktop in desktop_list:
+    #print desktop
+    if desktop[0] == '#': # focused desktop right now
+        # blue on yellow
+        #total_string += "%{F#99002b36}%{B#ffB58900}" + desktop + "%{B-}%{F-} "
+    
+        # white on green
+        total_string += "%{F#ffffffff}%{B#ff859900}" + " " + desktop[1] + " " + "%{B-}%{F-} "
+   
+    elif desktop[0] == '+':
+        total_string += "%{F#ffffffff}%{B#ff9CA668}" + " " + desktop[1] + " " + "%{B-}%{F-} "
+   
+    elif desktop[0] == ':':
+        total_string += "%{F#ffffffff}%{B#ff6A4100}" + " " + desktop[1] + " " + "%{B-}%{F-} "
+   
+    elif desktop[0] == '!':
+        total_string += "%{F#ffffffff}%{B#ffFF0675}" + " " + desktop[1] + " " + "%{B-}%{F-} "
+   
+    else:
+        total_string += "%{F#ffffffff}%{B#ff045F70}" + " " + desktop[1] + " " + "%{B-}%{F-} "
+print total_string
